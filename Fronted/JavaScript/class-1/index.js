e1de817ae88741ab440481014d710b7c17355e0e
anjankumardas5 <anjantechnology507@gmail.com>
2024-10-01 23:23:07 +0530

    Day 2, Data types

--- Fronted/JavaScript/class-1/index.js
+++ Fronted/JavaScript/class-1/index.js
@@ -5,3 +5,19 @@ console.log(b);
 const c=4;
 console.log(c);
 
+
+// class 2
+//node const city="Balasore";
+console.log(address);
+var address="Balasore";
+console.log(address);
+
+//let user;
+let  user=null;
+console.log(user)
+user="Anjan";
+console.log(user)
+console.log(typeof user)//object
+
+let d=0;
+console.log(typeof a);
