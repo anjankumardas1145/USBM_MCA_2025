0098417120fb42ddff4f23ef83295967986d57b2
anjankumardas5 <anjantechnology507@gmail.com>
2024-10-07 17:18:41 +0530

    Quiestions

---
+++ Fronted/JavaScript/class-2/Ques.js
@@ -0,0 +1,26 @@
+
+//Question:1
+let foodPrice = 200;   
+let drinkPrice = 150;
+let Bill = foodPrice + drinkPrice;//350
+let tip = Bill * 0.10;//35
+
+
+let finalAmount = Bill + tip;//350+35=385
+
+
+console.log(`The final amount is :${finalAmount}`);
+
+
+//Question :2
+let originalPrice=500;
+let discount =200;
+let finalPrice=originalPrice-discount;
+console.log(`The final price of the product is :${finalPrice}`);
+
+//question:3
+let productPrice=500;
+let taxRate=8;
+let taxAmount=(productPrice*taxRate)/100;//(500*8)/100 i.e 40
+let totalAmount=productPrice+taxAmount;//500+40=540
+console.log (`After sale the total price is : ${totalAmount}`)
