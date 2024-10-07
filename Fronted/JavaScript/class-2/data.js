779eddf992e656398868a379b14c7bd8b3897d15
anjankumardas5 <anjantechnology507@gmail.com>
2024-10-07 11:12:48 +0530

    today task for addition,sub,mul,div

---
+++ Fronted/JavaScript/class-2/data.js
@@ -0,0 +1,36 @@
+
+//undefind
+
+let data;
+console.log(typeof data);
+console.log(data);
+
+//number
+let age=22;
+console.log("My age is : ",age);
+console.log(typeof age);
+let pi=3.14;
+console.log(pi);
+console.log(typeof pi);
+//let num=08; error because decimal with leading zero are not allowed here
+
+//Addition
+let num1=11;
+let num2=4;
+let addition=num1+num2;
+console.log(addition)
+//sub
+num1=11;
+ num2=4;
+let sub=num1-num2;
+console.log(sub);
+
+ num1=11;
+num2=4;
+ let mul=num1*num2;
+console.log(mul);
+
+ num1=11;
+num2=4;
+let div=num1/num2;
+console.log(div);
