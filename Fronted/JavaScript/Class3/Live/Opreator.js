83f038ad1b0ec36dc1d2026892af5c8a3cc87ed5
anjankumardas5 <anjantechnology507@gmail.com>
2024-10-08 11:49:45 +0530

    task1

---
+++ Fronted/JavaScript/Class3/Live/Opreator.js
@@ -0,0 +1,65 @@
+// Mathmatical operator
+// let num1=5;
+// let num2=3;
+// let num3=4;
+// let sum=num1+ num2+num3;
+
+// //% operator
+// let num=29;
+// let odd=num%2;
+// console.log(odd);
+
+// //Exponential operator
+// let squre=2*2;
+// let cube=2*2*2;
+//console.log(squre,cube);
+
+//Compare two values
+// console.log(10>5);//true
+// console.log(4>5);
+// console.log(7>!5);//true
+// console.log(5>=5);//true
+// console.log(5>5);false
+//  console.log(!5);
+
+ //Equality operator
+ // '==' :only check value
+ // '===' :value and type check
+
+//  console.log(5==5);//true
+//  console.log(5==6);//false
+//  console.log(5=="5")//true
+//  console.log(5==="5");//false
+//  console.log(5!="5");//false
+//  console.log(5!=="5");//true
+
+
+ //CONDITIONAL Operatoe
+//  if(condition){
+//   //true value executed here
+//  }
+
+ //false value
+ //false,0,-0, "",' ', null,indefing non
+
+
+
+ if(false){
+  console.log("Anjan");
+
+ }
+
+ if(""){
+  console.log("Ranjan");// no space in double quote
+ }
+ if(" "){
+  console.log("Ram")
+ }
+
+
+ let total_bill=500;
+ if(total_bill>300){// if your bill is more then 300 then you will get discount
+  console.log("congrats you got discount");
+ }else{
+  console.log(" sry  you have no discount ")
+ }
