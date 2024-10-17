6d8d17ef3d038bfec19cf862cb093d99c04be92a
anjankumardas5 <anjantechnology507@gmail.com>
2024-10-17 23:44:16 +0530

    loop in js

---
+++ Fronted/JavaScript/class5/index.js
@@ -0,0 +1,24 @@
+console.log("Jay jagannath 🙏")
+//print 1 -10
+console.log("Manually");
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log("Mun 10 print karibini")
+console.log(11);
+
+//while loop
+ let num=1;
+ while(num<=10){
+  console.log(num);
+  num++;
+ }
+ console.log(" global",num)
+
+ 
