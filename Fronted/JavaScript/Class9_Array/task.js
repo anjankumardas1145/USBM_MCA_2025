41cd0036765a76df7450ad09542d1517bf1b1c55
anjankumardas5 <anjantechnology507@gmail.com>
2024-10-29 03:04:42 +0530

    use of function like push(),pop() etc ...

---
+++ Fronted/JavaScript/Class9_Array/task.js
@@ -0,0 +1,47 @@
+// USE OF push():Generally push() is used to add some element to an exiting array at the end position.
+
+let cricketers=new Array("Rohit","virat","yuvraj");
+console.log(cricketers);
+cricketers.push("Rahul");
+console.log(cricketers);
+
+
+//Use of pop(): it is method used to remove the last element from the array 
+console.log("For pop() function ")
+
+let Cricketers=["Rohit"," Virat","Yuvraj"];
+console.log(Cricketers);
+Cricketers.pop();
+console.log(Cricketers);
+
+
+//shift(): it is used to remove the first element from the array
+
+console.log("For shift() function ")
+let cRicketers=["Rohit","virat","yuvraj"];
+console.log(cricketers);
+cRicketers.shift();
+console.log(cRicketers);
+
+//use of unshift(): it is a methos used add element at the beginning of the array
+
+console.log("For unshift() function ")
+let dRicketers=["Rohit","virat","Yuvraj"];
+console.log(dRicketers);
+dRicketers.unshift("Hardik");
+console.log(dRicketers);
+
+
+//reverse():It is method in js used to return the element in reverse order
+
+console.log("For reverse() function ")
+
+let eRicketers=["Rohit","virat","yuvraj"];
+console.log(eRicketers);
+eRicketers.reverse();
+console.log(eRicketers);
+
+
+
+
+  
