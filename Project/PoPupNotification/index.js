155313add93f51103803e99e026a55b6260f026c
anjankumardas5 <anjantechnology507@gmail.com>
2024-10-30 00:21:55 +0530

    poPup Notifivation

---
+++ Project/PoPupNotification/index.js
@@ -0,0 +1,13 @@
+ showNotification=(()=> {
+  const notification = document.getElementById("notification");
+
+//   
+
+//   // Hide the notification after 5 seconds
+
+// 
+setTimeout(()=> {
+  notification.style.display = "none";
+ // notification.innerHTML=null;
+    }, 3000);
+ })
